ddc19fba1294ad607f8338cb6c0048436924e6ea
Mathias Blum <mathias.blum@student.tugraz.at>
2023-01-09 17:41:09 +0100

    Added static file serving to host frontend files.

--- server.js
+++ server.js
@@ -25,6 +25,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// INFO: this is from cyclic html fullstack template
+// #############################################################################
+// This configures static hosting for files in /public that have the extensions
+// listed in the array.
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+  index: ['index.html'],
+  maxAge: '1m',
+  redirect: false
+}
+app.use(express.static('dist/', options))
+
 // use JWT auth to secure the api
 app.use(jwt());
 
