d6e79bd43a8321e51a6367c1f5af8248dfe1e993
Mathias Blum <mathias.blum@student.tugraz.at>
2023-05-30 10:30:01 +0200

    Test

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const app = require('./app');
-const db = require('./_helpers/db');
+// const db = require('./_helpers/db');
 
 const appPort = process.env.PORT || 8000;
 const adminEmail = process.env.ADMIN_EMAIL || 'admin';
 const adminPwd = process.env.ADMIN_PWD || 'admin189m';
 
 // Connect to the database before listening
-db.connectDB().then(() => {
+// db.connectDB().then(() => {
   app.listen(appPort, () => {
     console.log(`Example app listening at http://localhost:${appPort}`);
   });
-});
+// });
