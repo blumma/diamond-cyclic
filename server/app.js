8926bfae8c414376b711af89b93dfd3cc854e5b3
Mathias Blum <mathias.blum@student.tugraz.at>
2023-05-30 09:28:05 +0200

    Changed start script.

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
-// const jwt = require('./_helpers/jwt');
-// const errorHandler = require('./_helpers/error-handler');
+const jwt = require('./_helpers/jwt');
+const errorHandler = require('./_helpers/error-handler');
 const accountController = require('./src/controllers/account.controller');
 
 const app = express();
@@ -31,14 +31,16 @@ var options = {
 const publicDir = process.env.PUBLIC_DIR || '../client/dist/client';
 app.use(express.static(publicDir, options));
 
-// app.use(cors());
+app.use(cors());
 
 const maxPayloadSize = process.env.MAX_REQUEST_PAYLOAD_SIZE || '6mb';
 app.use(bodyParser.urlencoded({ extended: false, limit: maxPayloadSize }));
 app.use(bodyParser.json({ limit: maxPayloadSize }));
 
+app.use(jwt());
+
 app.use('/users', accountController);
 
-// app.use(errorHandler);
+app.use(errorHandler);
 
 module.exports = app
